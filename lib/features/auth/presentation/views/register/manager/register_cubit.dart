e0c4d98d33ece0a7de66825695ab301662b9c6d4
AmerDje <amer.dje10n20@gmail.com>
2024-05-11 00:39:56 +0100

    chore: add register and email verification

---
+++ lib/features/auth/presentation/views/register/manager/register_cubit.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hakawati/features/auth/data/models/user.dart';
+import 'package:hakawati/features/auth/data/repository/auth_repository.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  final AuthRepository authRepository;
+  RegisterCubit(this.authRepository) : super(RegisterInitial());
+
+  Future<void> register(
+      {required String email,
+      required String name,
+      required String phone,
+      required String password,
+      required String locate}) async {
+    emit(RegisterLoading());
+    final result = await authRepository.register(email, password, locate);
+    result.fold((failure) => emit(RegisterFailure(errMessage: failure.message)), (firebaseUser) async {
+      final userModel = UserModel(
+        uid: firebaseUser.uid,
+        password: password,
+        email: email,
+        name: name,
+        emailVerified: firebaseUser.emailVerified,
+        phoneNumber: phone,
+        locate: locate,
+        photoUrl: 'https://img.freepik.com/premium-vector/bearded-smiling-man-with-arms-crossed_165429-132.jpg?w=740',
+      );
+      final result = await authRepository.createUser(userModel);
+      result.fold(
+          (failure) => RegisterFailure(errMessage: failure.message), (user) => emit(RegisterSuccess(user: userModel)));
+    });
+  }
+
+  Future<void> updateUser(UserModel user) async {
+    emit(RegisterLoading());
+    final result = await authRepository.updatePersonalData(user);
+    result.fold(
+      (failure) => RegisterFailure(errMessage: failure.message),
+      (isUpdated) => debugPrint("User Update $isUpdated"),
+    );
+  }
+
+  Future<void> sendEmailVerification() async {
+    emit(VerificationLoading());
+    var result = await authRepository.sendEmailVerification();
+    result.fold((failure) => emit(VerificationFailure(errMessage: failure.message)), (success) async {
+      emit(VerificationEmailSent());
+      await setTimerForEmailVerification();
+    });
+  }
+
+  Future<void> setTimerForEmailVerification() async {
+    Timer.periodic(const Duration(seconds: 3), (timer) async {
+      var result = await authRepository.reloadUserStatus();
+      result.fold((failure) => emit(VerificationFailure(errMessage: failure.message)), (verified) {
+        if (verified) {
+          emit(VerificationSuccess());
+          timer.cancel();
+        }
+      });
+    });
+  }
+
+  Future<void> deleteUser() async {
+    emit(DeleteUserLoading());
+    var result = await authRepository.deleteUser();
+    result.fold(
+        (failure) => emit(DeleteUserFailure(errMessage: failure.message)), (success) => emit(DeleteUserSuccess()));
+  }
+}
