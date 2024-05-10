08e3521dbe5b71616e20b095e1778f78765146ba
AmerDje <amer.dje10n20@gmail.com>
2024-05-11 00:37:22 +0100

    chore: add auth implementation

--- lib/core/service/service_locator.dart
+++ lib/core/service/service_locator.dart
@@ -1,15 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hakawati/core/api/dio_consumer.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:hakawati/core/network/connectivity_checker.dart';
+import 'package:hakawati/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:hakawati/features/auth/presentation/views/register/manager/register_cubit.dart';
 
 final sl = GetIt.instance;
 
 class ServicesLocator {
   static void init() {
+    //Dio sl
     sl.registerLazySingleton<DioConsumer>(() => DioConsumer(sl.get<Dio>(), sl.get<ConnectivityCheckerImpl>()));
     sl.registerLazySingleton<ConnectivityCheckerImpl>(() => ConnectivityCheckerImpl(connectivity: Connectivity()));
     sl.registerLazySingleton<Dio>(() => Dio());
+
+    // Auth sl
+    sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+    sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+    sl.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
+    sl.registerLazySingleton<FacebookAuth>(() => FacebookAuth.instance);
+
+    // Repository sl
+    sl.registerLazySingleton<AuthRepositoryImpl>(() => AuthRepositoryImpl(
+        firebaseAuth: sl.get<FirebaseAuth>(),
+        firebaseFirestore: sl.get<FirebaseFirestore>(),
+        googleSignIn: sl.get<GoogleSignIn>(),
+        facebookAuth: sl.get<FacebookAuth>()));
+
+    // Cubit sl
   }
 }
