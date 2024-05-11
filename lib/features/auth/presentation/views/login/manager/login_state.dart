56a426757615c48c8d100f1ca6d861d5a2a16948
AmerDje <amer.dje10n20@gmail.com>
2024-05-11 01:46:02 +0100

    chore: add login implementation

---
+++ lib/features/auth/presentation/views/login/manager/login_state.dart
@@ -0,0 +1,34 @@
+part of 'login_cubit.dart';
+
+sealed class LoginState extends Equatable {
+  const LoginState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoading extends LoginState {}
+
+final class LoginSuccess extends LoginState {
+  final UserModel user;
+
+  const LoginSuccess({required this.user});
+}
+
+final class LoginFailure extends LoginState {
+  final String errMessage;
+
+  const LoginFailure({required this.errMessage});
+}
+
+final class LogoutLoading extends LoginState {}
+
+final class LogoutSuccess extends LoginState {}
+
+final class LogoutFailure extends LoginState {
+  final String errMessage;
+
+  const LogoutFailure({required this.errMessage});
+}
