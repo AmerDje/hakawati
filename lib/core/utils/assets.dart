7242958683a1cbf54f5daf5360e5246df2046cb6
AmerDje <amer.dje10n20@gmail.com>
2024-06-19 21:26:18 +0100

    chore: change onboarding assets

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,11 @@
 abstract class Assets {
-  static String get logo => 'assets/images/logo.png';
+  static const String logo = 'assets/images/logo.png';
+
+  static const String onboardingOne = 'assets/images/onboarding_1.png';
+  static const String onboardingTwo = 'assets/images/onboarding_2.png';
+  static const String onboardingThree = 'assets/images/onboarding_3.png';
+
+  static const String testImageOne = 'assets/images/test_image_1.png';
+  static const String testImageTwo = 'assets/images/test_image_2.png';
+  static const String testImageThree = 'assets/images/test_image_3.png';
 }
