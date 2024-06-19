57971bd76866c4780d7e4855ddcdae5f9593051d
AmerDje <amer.dje10n20@gmail.com>
2024-06-19 21:23:37 +0100

    chore: update flutter version & dependencies

--- lib/core/utils/extensions/media_query.dart
+++ lib/core/utils/extensions/media_query.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 extension MediaQueryValues on BuildContext {
-  double get height => MediaQuery.of(this).size.height;
-  double get width => MediaQuery.of(this).size.width;
+  double get height => MediaQuery.sizeOf(this).height;
+  double get width => MediaQuery.sizeOf(this).width;
   double get topPadding => MediaQuery.of(this).viewPadding.top;
   double get bottomInsets => MediaQuery.of(this).viewInsets.bottom;
 }
