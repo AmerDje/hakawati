7764bbf1ee4b580e977e197c1a6c6df5ec86303d
AmerDje <amer.dje10n20@gmail.com>
2024-07-20 00:00:31 +0100

    chore: finished upload profile image impl

--- lib/features/auth/data/repository/auth_repository.dart
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hakawati/core/error/failure.dart';
@@ -9,6 +11,8 @@ abstract class AuthRepository {
   Future<Either<Failure, UserModel>> fetchPersonalData(String? uid);
   Future<Either<Failure, User>> register(String email, String password, String locate);
   Future<Either<Failure, UserModel>> createUser(UserModel user);
+  Future<Either<Failure, bool>> updateUser(String userId, Map<String, dynamic> updates);
+  Future<Either<Failure, String>> uploadImage(File image);
   Future<Either<Failure, UserModel>> logout();
   Future<Either<Failure, UserModel>> passwordResetSubmit(String email);
   Future<Either<Failure, bool>> sendEmailVerification();
