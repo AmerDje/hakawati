617e7fed1e9466e13a41e193f0b15d11292c57ef
AmerDje <amer.dje10n20@gmail.com>
2024-07-19 17:50:02 +0100

    chore: added global bloc and base state

--- lib/core/service/service_locator.dart
+++ lib/core/service/service_locator.dart
@@ -1,14 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart' show WidgetsBinding;
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hakawati/core/api/dio_consumer.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:hakawati/core/common/global/bloc/global_bloc.dart';
 import 'package:hakawati/core/network/connectivity_checker.dart';
 import 'package:hakawati/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:hakawati/features/auth/presentation/manager/auth_cubit.dart';
 import 'package:hakawati/features/auth/presentation/views/register/manager/register_cubit.dart';
+import 'package:hakawati/features/settings/presentation/manager/settings_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -34,5 +38,9 @@ class ServicesLocator {
 
     // Cubit sl
     sl.registerFactory<RegisterCubit>(() => RegisterCubit(sl.get<AuthRepositoryImpl>()));
+    sl.registerFactory<GlobalBloc>(() => GlobalBloc());
+    sl.registerFactory<SettingsCubit>(
+        () => SettingsCubit(platformBrightness: WidgetsBinding.instance.platformDispatcher.platformBrightness));
+    sl.registerFactory<AuthCubit>(() => AuthCubit());
   }
 }
