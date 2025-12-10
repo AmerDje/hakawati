54bb2994f76fe2e41b2793dad2f270aa43335dce
AmerDje <amer.dje10n20@gmail.com>
2025-12-10 09:03:49 +0200

    chore: added dio interceptor

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -7,6 +7,7 @@ import 'package:get_it/get_it.dart';
 import 'package:hakawati/core/functions/logger.dart';
 import 'package:hakawati/core/global/temp/temp_cache_cubit.dart';
 import 'package:hakawati/core/services/dio_consumer.dart';
+import 'package:hakawati/core/services/dio_interceptor.dart';
 import 'package:hakawati/core/services/firebase_auth_service.dart';
 import 'package:hakawati/core/services/firebase_firestore_service.dart';
 import 'package:hakawati/core/services/firebase_storage_service.dart';
@@ -30,34 +31,40 @@ class ServicesLocator {
           connectTimeout: const Duration(milliseconds: 60000),
           receiveTimeout: const Duration(milliseconds: 60000),
         ),
-      )..interceptors.add(LogInterceptor(
-          error: false,
-          logPrint: (error) => avoidPrint(error),
-          request: false,
-          requestBody: true,
-          requestHeader: true,
-          responseHeader: false,
-          responseBody: true,
-        )),
+      )..interceptors.addAll([
+          DioInterceptor(),
+          LogInterceptor(
+            error: false,
+            logPrint: (error) => avoidPrint(error),
+            request: false,
+            requestBody: true,
+            requestHeader: true,
+            responseHeader: false,
+            responseBody: true,
+          )
+        ]),
     );
 
     sl.registerLazySingleton<DioConsumer>(() => DioConsumer(sl.get<Dio>()));
     // Auth sl
     // Firebase sl
     sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
-    sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+    sl.registerLazySingleton<FirebaseFirestore>(
+        () => FirebaseFirestore.instance);
     sl.registerLazySingleton<FirebaseStorage>(() => FirebaseStorage.instance);
 
     // Service sl
-    sl.registerLazySingleton<FirebaseFireStoreService>(() => FirebaseFireStoreService(
-          firestore: sl.get<FirebaseFirestore>(),
-        ));
+    sl.registerLazySingleton<FirebaseFireStoreService>(
+        () => FirebaseFireStoreService(
+              firestore: sl.get<FirebaseFirestore>(),
+            ));
     sl.registerLazySingleton<FirebaseAuthService>(() => FirebaseAuthService(
           firebaseAuth: sl.get<FirebaseAuth>(),
         ));
-    sl.registerLazySingleton<FirebaseStorageService>(() => FirebaseStorageService(
-          firebaseStorage: sl.get<FirebaseStorage>(),
-        ));
+    sl.registerLazySingleton<FirebaseStorageService>(
+        () => FirebaseStorageService(
+              firebaseStorage: sl.get<FirebaseStorage>(),
+            ));
 
     // Repository sl
     // Auth Repo sl
@@ -68,10 +75,12 @@ class ServicesLocator {
         ));
 
     // Cubit sl
-    sl.registerFactory<RegisterCubit>(() => RegisterCubit(sl.get<AuthRepository>()));
+    sl.registerFactory<RegisterCubit>(
+        () => RegisterCubit(sl.get<AuthRepository>()));
     sl.registerFactory<TempCacheCubit>(() => TempCacheCubit());
-    sl.registerFactory<SettingsCubit>(
-        () => SettingsCubit(platformBrightness: WidgetsBinding.instance.platformDispatcher.platformBrightness));
+    sl.registerFactory<SettingsCubit>(() => SettingsCubit(
+        platformBrightness:
+            WidgetsBinding.instance.platformDispatcher.platformBrightness));
     sl.registerFactory<AuthCubit>(() => AuthCubit());
   }
 }
